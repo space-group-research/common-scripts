8897a76d270e1441322fb823f8d1d1b707c24803
Angela <144486389+amshipman117@users.noreply.github.com>
2024-10-31 12:34:21 -0400

    Create cutoff.sh

---
+++ CP2K/cutoff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+base=`pwd`
+cutoffs=`seq -w 250 50 2000` #start at 50 Ry, make a new folder every 250 steps, ending at 2000 Ry
+rel_cutoff=60
+
+inp_file=`find . -maxdepth 1 -name '*.inp' | cut -c 3-` #Finds your input file and slices off characters to just have input file name.
+echo "Input file is ${inp_file}."
+
+    for cutoff in $cutoffs
+    do
+        mkdir -p cutoff
+        cd cutoff
+        mkdir -p $cutoff
+        cd $cutoff
+        cp $base/start-pos.xyz . #update if you're using a different file name for your atomic positions
+        cp $base/cp2k.sh . #update if your cp2k submit script is named differently
+        sed -e "s/LT_rel_cutoff/${rel_cutoff}/g" \
+            -e "s/LT_cutoff/${cutoff}/g" $base/${inp_file} > ${inp_file}
+        bsub < cp2k.sh
+        cd ../..
+    done
